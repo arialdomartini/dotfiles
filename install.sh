3fe27bf8333fcc0f5e719d638ce85f4fb2c9d14b
Arialdo Martini <arialdomartini@gmail.com>
2019-05-04 15:50:15 +0200

    No spacemacs

--- install.sh
+++ install.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-declare -a files=(".zshrc"  ".emacs"  ".spacemacs"  ".gitconfig"  ".gitignore-global"  "gradle.properties"  ".tmux.conf"  ".config/i3/config" ".tigrc")
+declare -a files=(".zshrc" ".gitconfig" ".gitignore-global" "gradle.properties" ".tmux.conf" ".config/i3/config" ".tigrc" ".emacs.d")
 
 function install() {
     for i in "${files[@]}"
     do
         ln -fs `pwd -P`/$i ~/$i
     done
-
-    ln -fs `pwd -P`/karabiner.json ~/.config/karabiner/karabiner.json
 }
-
-echo "Files that will be overwritten:"
-for i in "${files[@]}"
-do
-    echo "./$i => $HOME/$i"
-done
-
-echo "./karabiner.json => ~/.config/karabiner.json"
 
 read -p "Are you sure (y/n)? " reply
 
