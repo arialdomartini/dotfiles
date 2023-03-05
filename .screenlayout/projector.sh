2fda66231cb8f38fd6601d342e7e9e7b6eed6e96
Arialdo Martini <arialdomartini@gmail.com>
2023-03-05 23:17:54 +0100

    laptop and projector arandr profiles

---
+++ .screenlayout/projector.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --mode 1280x800 --pos 1920x280 --rotate normal --output DP2 --off --output VIRTUAL1 --off
