b3d93f66927dc0662852c497dbaefa77228c9c9d
Arialdo Martini <arialdomartini@gmail.com>
2023-03-12 10:51:07 +0100

    Emacs: GC optimized during startup

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -21,4 +21,17 @@
 
 (set-frame-font "Monoid 20")
 
+;; Native comp
+(setq
+ native-comp-speed 3
+ comp-speed 3)
+;;; Speed up boot time by increasing GC thresholds during startup only
+(setq
+ gc-cons-threshold (* 64 1024 )
+ gc-cons-percentage 0.6)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq
+	     gc-cons-threshold (* 64 1024 1024)
+	     gc-cons-percentage 0.1)))
 
