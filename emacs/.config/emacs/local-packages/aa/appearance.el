0d6dc9971e5f1533a6b9b5807b275489650127de
Arialdo Martini <arialdomartini@gmail.com>
2023-03-17 16:39:06 +0100

    Emacs: nyan-mode

--- emacs/.config/emacs/local-packages/aa/appearance.el
+++ emacs/.config/emacs/local-packages/aa/appearance.el
@@ -9,11 +9,13 @@
 (set-frame-font "Monoid 20")
 (load-theme 'adwaita)
 
-;; (use-package nyan-mode
-;;   :config
-;;   (nyan-mode 0)
-;;   (nyan-start-animation)
-;;   (nyan-toggle-wavy-trail))
+(unless (package-installed-p 'nyan-mode)
+  (package-vc-install "https://github.com/TeMPOraL/nyan-mode.git"))
+(use-package nyan-mode
+  :config
+  (nyan-mode 0)
+  (nyan-start-animation)
+  (nyan-toggle-wavy-trail))
 
 
 (pixel-scroll-precision-mode)
