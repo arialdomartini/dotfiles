8ecb84a5dca292e208ab0bbbcbdeb5a291a5f66c
Arialdo Martini <arialdomartini@gmail.com>
2023-03-18 07:58:45 +0100

    Emacs: consult+vertico+marginalia

--- emacs/.config/emacs/local-packages/aa/packages.el
+++ emacs/.config/emacs/local-packages/aa/packages.el
@@ -4,13 +4,11 @@
   :bind ("C-=" . er/expand-region))
 
 (use-package magit :ensure t)
-;;(use-package marginalia :ensure t)xo
-;;(use-package marginalia :ensure t)
-;; (use-package consult :ensure t)
+;; (use-package projectile :ensure t)
 ;; (use-package embark :ensure t)
 ;; (use-package embark-consult :ensure t)
 ;; (use-package orderless :ensure t)
-;; (use-package projectile :ensure t)
+
 ;; (use-package which-key :ensure t)
 ;; (use-package git-gutter :ensure t)
 ;; (use-package git-time-machine :ensure t)
@@ -19,6 +17,51 @@
 ;; ;;(use-package vterm :ensure t)
 ;; ;;(use-package drag-stuff :ensure t)
 
+
+(use-package consult
+  :ensure t
+  :bind (("M-g M-g" . consult-goto-line)
+	 ("C-x b" . consult-buffer)
+	 ("C-s"   . consult-line-multi)
+	 ("C-S-s" . isearch-forward)
+	 ("C-x r b" . consult-bookmark)))
+(setq register-preview-delay 0.5
+      register-preview-function #'consult-register-format)
+
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode))
+
+(use-package marginalia
+  :ensure t
+  :init
+  (marginalia-mode))
+
+;; (use-package embark :ensure t)
+;; (use-package embark-consult :ensure t)
+;; (use-package orderless :ensure t)
+
+;; (use-package which-key :ensure t)
+;; (use-package embark :ensure t)
+;; (use-package embark-consult :ensure t)
+;; (use-package orderless :ensure t)
+
+;; (use-package which-key :ensure t)
+;; (use-package git-gutter :ensure t)
+;; (use-package git-time-machine :ensure t)
+;; (use-package ripgrep :ensure t)
+;; (use-package all-the-icons :ensure t)
+;; ;;(use-package vterm :ensure t)
+;; ;;(use-package drag-stuff :ensure t)
+
+
+;; (use-package git-gutter :ensure t)
+;; (use-package git-time-machine :ensure t)
+;; (use-package ripgrep :ensure t)
+;; (use-package all-the-icons :ensure t)
+;; ;;(use-package vterm :ensure t)
+;; ;;(use-package drag-stuff :ensure t)
 
 
 (provide 'aa/packages)
