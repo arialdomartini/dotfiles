579db414b918298285f20d91e51446e46b62a1f0
Arialdo Martini <arialdomartini@gmail.com>
2023-04-13 23:28:44 +0200

    Emacs: embark with C-. C-,

--- emacs/.config/emacs/local-packages/aa/packages.el
+++ emacs/.config/emacs/local-packages/aa/packages.el
@@ -5,16 +5,21 @@
 
 (use-package magit :ensure t)
 ;; (use-package projectile :ensure t)
-;; (use-package embark :ensure t)
+(use-package embark
+  :ensure t
+  :bind (("C-." . embark-act)
+	 ("C-," . embark-dwim)))
+
 ;; (use-package embark-consult :ensure t)
 ;; (use-package orderless :ensure t)
 
 (use-package which-key :ensure t)
+
 ;; (use-package git-gutter :ensure t)
 ;; (use-package git-time-machine :ensure t)
 ;; (use-package ripgrep :ensure t)
 ;; (use-package all-the-icons :ensure t)
-;; ;;(use-package drag-stuff :ensure t)
+;;(use-package drag-stuff :ensure t)
 
 (use-package consult
   :ensure t
