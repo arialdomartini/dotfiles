4f77c2ffdea707c0e5ead853d27ae7210f6454db
Arialdo Martini <arialdomartini@gmail.com>
2023-04-19 23:42:59 +0200

    Emacs: dashboard, with Doom logo

---
+++ emacs/.config/emacs/local-packages/aa/dashboard.el
@@ -0,0 +1,27 @@
+(use-package page-break-lines
+  :ensure t
+  :config
+  (global-page-break-lines-mode))
+
+(use-package dashboard
+  :ensure t
+  :init
+  (setq
+   dashboard-startup-banner (concat user-emacs-directory "logo.svg")
+   dashboard-image-banner-max-width 250
+   dashboard-banner-logo-title nil
+   dashboard-center-content t)
+  
+  :config
+  (dashboard-setup-startup-hook)
+  (text-scale-set 1)
+  (setq dashboard-items '((recents . 7)
+                          (bookmarks . 3)
+                          (projects . 5))
+	text-scale .2))
+
+;; (custom-set-faces
+;;   '(dashboard-heading-face ((t (:inherit font-lock-header-face :weight bold :height .5))))
+;;   '(dashboard-items-face ((t (:inherit font-lock-keyword-face :height .2)))))
+
+(provide 'aa/dashboard)
