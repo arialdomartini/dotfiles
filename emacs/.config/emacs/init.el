5aaa270889c9f3a3d2b17b1d991d20cc7bf240c9
Arialdo Martini <arialdomartini@gmail.com>
2023-05-11 19:24:29 +0200

    Emacs: packages are scanned from a directory

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,6 +1,8 @@
 ;; _*_ lexical-binding: t _*_
 
-(defun aa/get-packages (path)
+(load (locate-user-emacs-file "aa-prelude.el"))
+
+(defun aa-get-packages (path)
   (let ((files (directory-files-recursively path "")))
     (mapcar (lambda (f)
 	      (intern
@@ -8,36 +10,13 @@
 		(file-relative-name f path))))
 	    files)))
 
-(defun aa/require-packages-in (path)
-  (let ((packages (aa/get-packages path)))
+(defun aa-require-packages-in (path)
+  (let ((packages (aa-get-packages path)))
     (add-to-list 'load-path path)
     (dolist(p packages)
       (require p))))
 
-;(aa/require-packages-in
-; (expand-file-name "local-packages" user-emacs-directory))
 
-
-(add-to-list 'load-path (locate-user-emacs-file "local-packages"))
-(dolist(p '(aa-prelude
-	    aa-defaults
-	    aa-packages
-	    aa-appearance
-	    aa-backup
-	    aa-dashboard
-	    aa-font
-	    aa-icons
-	    aa-lisp
-	    aa-mode-line
-	    aa-nativecomp
-	    aa-projectile
-	    aa-scheme
-	    aa-theme
-	    aa-windows))
-    (require p))
-
-
-
-
+(aa-require-packages-in (locate-user-emacs-file "local-packages"))
 
 (put 'dired-find-alternate-file 'disabled nil)
