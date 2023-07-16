1007e7106b5084644812489960f1508b897c620c
Arialdo Martini <arialdomartini@gmail.com>
2023-07-16 14:46:22 +0200

    notmuch: no logo, auto-refresh

--- emacs/.config/emacs/local-packages/aa-email.el
+++ emacs/.config/emacs/local-packages/aa-email.el
@@ -2,6 +2,12 @@
 (use-package notmuch
   :ensure nil ; because it comes with the `notmuch' from Arch
   :config
-  (define-key global-map (kbd "C-c m") #'notmuch))
+  (define-key global-map (kbd "C-c m") #'notmuch)
+  (setq notmuch-show-logo nil
+	notmuch-search-oldest-first nil
+	notmuch-hello-auto-refresh t))
 
 (provide 'aa-email)
+
+
+
