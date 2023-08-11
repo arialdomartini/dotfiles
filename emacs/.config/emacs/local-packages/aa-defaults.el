7fe5d1ceeec1f41ce6999f098393988986073777
Arialdo Martini <arialdomartini@gmail.com>
2023-08-11 19:30:28 +0200

    Emacs: prot-simple-keyboard-quit-dwim

--- emacs/.config/emacs/local-packages/aa-defaults.el
+++ emacs/.config/emacs/local-packages/aa-defaults.el
@@ -34,4 +34,34 @@
   :config
   (dirvish-override-dired-mode))
 
+(progn
+  ;; copied from Prot's `prot-simple-keyboard-quit-dwim'
+  (defun aa-simple-keyboard-quit-dwim ()
+    "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+    (interactive)
+    (cond
+     ((region-active-p)
+      (keyboard-quit))
+     ((derived-mode-p 'completion-list-mode)
+      (delete-completion-window))
+     ((> (minibuffer-depth) 0)
+      (abort-recursive-edit))
+     (t
+      (keyboard-quit))))
+
+  (define-key global-map [remap keyboard-quit] 'aa-simple-keyboard-quit-dwim) )
+
+
+
 (provide 'aa-defaults)
