0f459cc14c742260e7c83a10e3638a6244455267
Arialdo Martini <arialdomartini@gmail.com>
2023-08-31 17:11:14 +0200

    Emacs: custom.el is not loaded

--- emacs/.config/emacs/local-packages/aa-defaults.el
+++ emacs/.config/emacs/local-packages/aa-defaults.el
@@ -1,7 +1,5 @@
 ;; prevent Emacs from modifying configuration files
 (setq custom-file (make-temp-file "emacs-custom-"))
-(when (and custom-file (file-exists-p custom-file))
-   (load-file custom-file))
 
 (setq warning-minimum-level :error)
 
