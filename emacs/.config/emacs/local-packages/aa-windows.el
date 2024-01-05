cbb7721fc155993e26dcf59ad556e85c4878aad5
Arialdo Martini <arialdomartini@gmail.com>
2024-01-05 14:20:32 +0100

    Emacs: No auto-resize of windows

--- emacs/.config/emacs/local-packages/aa-windows.el
+++ emacs/.config/emacs/local-packages/aa-windows.el
@@ -3,7 +3,7 @@
   :config
   (setq switch-window-shortcut-style 'qwerty)
   (global-set-key (kbd "C-x o") 'switch-window)
-  (setq switch-window-auto-resize-window t)
+  (setq switch-window-auto-resize-window nil)
   (setq switch-window-default-window-size 0.65) ;auto resize to 65% of frame size
   (switch-window-mouse-mode)) ;auto resize with mouse too
 
