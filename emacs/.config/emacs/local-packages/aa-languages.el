8feb0dc3f92e2b3158157ad26765995792f118e6
Arialdo Martini <arialdomartini@gmail.com>
2024-02-19 21:36:11 +0100

    Emacs: aggressive-indent-mode

--- emacs/.config/emacs/local-packages/aa-languages.el
+++ emacs/.config/emacs/local-packages/aa-languages.el
@@ -20,6 +20,14 @@
     (setq eglot-fsharp-server-install-dir "~/.dotnet/tools/")
     (add-hook 'fsharp-mode-hook #'eglot-ensure)))
 
+(use-package aggressive-indent
+  :ensure t
+  :config
+  (defun aa-turn-aggressive-indent-mode-on ()
+    (aggressive-indent-mode t)
+    (message "Let's be aggressive! ARRR"))
+  (add-hook 'emacs-lisp-mode-hook #'aa-turn-aggressive-indent-mode-on))
+
 (use-package markdown-toc
   :ensure t)
 
