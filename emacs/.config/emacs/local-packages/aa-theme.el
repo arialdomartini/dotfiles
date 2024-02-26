48939a7aca7fccfc3434c44bc08d27fc577a8e78
Arialdo Martini <arialdo.martini@gmail.com>
2024-02-26 12:54:29 +0100

    Emacs: org source blocks are gray

--- emacs/.config/emacs/local-packages/aa-theme.el
+++ emacs/.config/emacs/local-packages/aa-theme.el
@@ -5,6 +5,7 @@
     (set-face-attribute 'mode-line nil          :box nil :underline nil  :overline nil)
     (set-face-attribute 'mode-line-inactive nil :box nil :underline nil  :overline nil))
   (add-hook 'enable-theme-functions 'aa-borderless-line)
+  (setq modus-themes-org-blocks 'gray-background)
 
   (modus-themes-load-theme 'modus-operandi-tinted))
 
