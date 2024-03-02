d552ace47c02ecf562e9bcde024b42e07ee9a7a1
Arialdo Martini <arialdo.martini@gmail.com>
2024-03-02 21:59:11 +0100

    arandr: laptop, projector, studio

---
+++ screenlayouts/.config/screenlayouts/projector.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 419x800 --rotate normal --output DP1 --mode 1280x800 --pos 0x0 --rotate normal --output DP1-1 --off --output DP1-2 --off --output DP1-3 --off --output DP2 --off --output VIRTUAL1 --off
