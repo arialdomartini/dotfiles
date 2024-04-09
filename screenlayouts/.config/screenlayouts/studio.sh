7cf1198ee56dac535bc1863fcd6aadd5030e09d8
Arialdo Martini <arialdo.martini@gmail.com>
2024-04-09 13:00:59 +0200

    arands: Studio mode has only external monitor, no laptop monitor

--- screenlayouts/.config/screenlayouts/studio.sh
+++ screenlayouts/.config/screenlayouts/studio.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 2560x0 --rotate normal --output DP1 --off --output DP1-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1-2 --off --output DP1-3 --off --output DP2 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --off --output DP1 --off --output DP1-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1-2 --off --output DP1-3 --off --output DP2 --off --output VIRTUAL1 --off
