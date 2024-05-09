ab5578a91f06d0b30757ebdb682b380a65dfdb49
Arialdo Martini <arialdo.martini@gmail.com>
2024-05-09 09:24:38 +0200

    Emacs: fsharp-mode with bugfix for fsautocomplete path

--- emacs/.config/emacs/local-packages/aa-languages.el
+++ emacs/.config/emacs/local-packages/aa-languages.el
@@ -11,14 +11,19 @@
   :config
   (add-hook 'haskell-mode-hook #'eglot-ensure))
 
-(progn
-  (use-package fsharp-mode
-    :ensure t)
-  (use-package eglot-fsharp
-    :ensure t
-    :config
-    (setq eglot-fsharp-server-install-dir "~/.dotnet/tools/")
-    (add-hook 'fsharp-mode-hook #'eglot-ensure)))
+(use-package fsharp-mode
+  :ensure t)
+(use-package eglot-fsharp
+  :ensure t
+  :after fsharp-mode
+  :config
+  ;; The original function used to prefix "dotnet" to the fsautocomplete path
+  (defun eglot-fsharp--path-to-server ()
+    "Return FsAutoComplete path."
+    (file-truename (concat eglot-fsharp-server-install-dir "fsautocomplete" (if (eq system-type 'windows-nt) ".exe" ""))))
+
+  (setq eglot-fsharp-server-install-dir "~/.dotnet/tools/")
+  (add-hook 'fsharp-mode-hook #'eglot-ensure))
 
 (use-package aggressive-indent
   :ensure t
