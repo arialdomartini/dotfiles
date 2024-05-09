6f52dfabf8207a80b49949e89dde6b6527c97152
Arialdo Martini <arialdo.martini@gmail.com>
2024-05-09 09:22:29 +0200

    Emacs: nerd-icons instead of all-the-icons

--- emacs/.config/emacs/local-packages/aa-icons.el
+++ emacs/.config/emacs/local-packages/aa-icons.el
@@ -1,12 +1,26 @@
-(use-package all-the-icons
-  :ensure t
-  :config
-  (require 'all-the-icons))
 
-(use-package all-the-icons-dired
+
+;; Icons
+(use-package nerd-icons
+  :ensure t)
+
+(use-package nerd-icons-completion
   :ensure t
+  :after marginalia
   :config
-    (setq all-the-icons-dired-monochrome nil) ;; does not work...
-  :hook ((dired-mode) . all-the-icons-dired-mode))
+  (nerd-icons-completion-marginalia-setup)
+  (nerd-icons-completion-mode 1))
+
+(use-package nerd-icons-corfu
+  :ensure t
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
+(use-package nerd-icons-dired
+  :ensure t
+  :hook
+  (dired-mode . nerd-icons-dired-mode)) 
+
 
 (provide 'aa-icons)
