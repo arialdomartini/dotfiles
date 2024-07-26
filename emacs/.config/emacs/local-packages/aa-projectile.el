393bb67e52df2cf7421143bc4fdc867f47112dec
Arialdo Martini <arialdo.martini@gmail.com>
2024-07-26 07:28:54 +0200

    Emacs: trying to disable Projectile

--- emacs/.config/emacs/local-packages/aa-projectile.el
+++ emacs/.config/emacs/local-packages/aa-projectile.el
@@ -1,10 +1,10 @@
-(use-package projectile
-  :ensure t
-  :config
-  (projectile-mode +1)
-  (setq projectile-project-search-path '(("~/prg/" . 2)))
-  :bind (:map projectile-mode-map
-              ("C-c p" . projectile-command-map)))
+;; (use-package projectile
+;;   :ensure t
+;;   :config
+;;   (projectile-mode +1)
+;;   (setq projectile-project-search-path '(("~/prg/" . 2)))
+;;   :bind (:map projectile-mode-map
+;;               ("C-c p" . projectile-command-map)))
 
 (use-package ag
   :ensure t)
