9b6a96d015d983b9bfa063c271fdf25b3c093555
Arialdo Martini <arialdo.martini@gmail.com>
2024-07-27 08:30:01 +0200

    Emacs: no commented out code in prelude.el

--- emacs/.config/emacs/aa-prelude.el
+++ emacs/.config/emacs/aa-prelude.el
@@ -1,10 +1,7 @@
 (require 'package)
+
 ;; TODO it seems not to have effect
 (setq use-package-always-ensure t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-
-;; TODO how to prevent this for loading all the times?
-;; (unless package-archive-contents
-;;  (package-refresh-contents))
 
 (provide 'aa-prelude)
