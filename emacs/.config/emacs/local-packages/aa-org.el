c096554ac76f15e83b74f1c6ea0666f8f6b6f76e
Arialdo Martini <arialdo.martini@gmail.com>
2024-07-27 09:50:50 +0200

    Emacs: Org uses auto-fill-mode

--- emacs/.config/emacs/local-packages/aa-org.el
+++ emacs/.config/emacs/local-packages/aa-org.el
@@ -1,7 +1,7 @@
 (use-package org
-  :ensure t
   :after olivetti
   :config
-  (add-hook 'org-mode-hook #'olivetti-mode))
+  (add-hook 'org-mode-hook #'olivetti-mode)
+  (add-hook 'org-mode-hook #'auto-fill-mode))
 
 (provide 'aa-org)
