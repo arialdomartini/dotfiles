2c3d9d05ae31e1060f401266800bf7a01ecc0830
Arialdo Martini <arialdo.martini@gmail.com>
2024-10-19 20:57:34 +0200

    Emacs: org support for bash

--- emacs/.config/emacs/local-packages/aa-org.el
+++ emacs/.config/emacs/local-packages/aa-org.el
@@ -1,7 +1,12 @@
 (use-package org
   :after olivetti
+  :defer
   :config
   (add-hook 'org-mode-hook #'olivetti-mode)
-  (add-hook 'org-mode-hook #'auto-fill-mode))
+  (add-hook 'org-mode-hook #'auto-fill-mode)
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((shell . t))))
 
 (provide 'aa-org)
