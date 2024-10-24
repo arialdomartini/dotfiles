c4b6b8510e39e4ec5f7baa116510bd82b16124a4
Arialdo Martini <arialdo.martini@gmail.com>
2024-10-24 22:39:01 +0200

    Home Manager: git package and configuration

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -6,55 +6,25 @@
 
   home.stateVersion = "24.05";
 
+  # imports = [
+  #   ./firefox.nix
+  # ];
+  
   programs = {
-    emacs = {
+    git = {
       enable = true;
-      package = (pkgs.emacs29-pgtk.override {
-        withTreeSitter = true;
-        withPgtk = false;
-      }).overrideAttrs (oldAttrs: {
-        configureFlags = (oldAttrs.configureFlags or []) ++ [
-          "--with-cairo"
-        ];
-      });
-      extraPackages = epkgs: [
-        pkgs.tree-sitter
-        pkgs.gnome-settings-daemon
-        pkgs.libcanberra-gtk3
-      ];
     };
   };
 
   home.packages = with pkgs; [
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    # TODO find a a way to move these files in program.git
+    xdg.configFile."git/config".source = ../../git/config;
+    xdg.configFile."git/.gitignore-global".source = ../../git/.gitignore-global;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  home.file = {
   };
 
   # Home Manager can also manage your environment variables through
