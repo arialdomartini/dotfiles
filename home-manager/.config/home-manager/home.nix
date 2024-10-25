b39266be6701f18f8b308d698e37ecbef60c7cee
Arialdo Martini <arialdo.martini@gmail.com>
2024-10-25 18:56:10 +0200

    Home Manager: no comments; all programs in the same section

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -12,6 +12,7 @@
   # ];
   
   programs = {
+    home-manager.enable = true;
     git = {
       enable = true;
     };
@@ -28,26 +29,7 @@
   home.file = {
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.hs
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/arialdo/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "emacs";
   };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
