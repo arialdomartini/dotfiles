4e324174aee3dfc8fb198bde7a2408e29ef1a5ff
Arialdo Martini <arialdo.martini@gmail.com>
2024-11-03 11:25:50 +0100

    Emacs: Org starts in overview mode

--- emacs/.config/emacs/local-packages/aa-org.el
+++ emacs/.config/emacs/local-packages/aa-org.el
@@ -4,7 +4,8 @@
   :config
   (add-hook 'org-mode-hook #'olivetti-mode)
   (add-hook 'org-mode-hook #'auto-fill-mode)
-
+  (setq org-startup-folded 'overview)
+  
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((shell . t))))
